670574a172c94b8228b1f924b2f8396a51c7a42a
ciisiq <cii_3@hotmail.com>
2023-12-12 18:04:44 +0000

    Feat: Add validate email

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
-console.log("hi");
+const input = document.getElementById('email');
+const submitbtn = document.getElementById('submit');
+const form = document.getElementById('form');
+const errorMessage = document.getElementById('valid-email');
+const sucess = document.getElementById('sucess');
+const whiteContainer = document.getElementById('white-container');
+
+form.addEventListener('submit', hadleClick);
+
+function hadleClick(event) {
+  event.preventDefault();
+  let email = input.value;
+  validateEmail(email);
+}
+
+function validateEmail(email) {
+  let validRegex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+  if (email.match(validRegex)) {
+    input.classList.remove('error');
+    errorMessage.style.display = 'none';
+    form.reset();
+    whiteContainer.style.display = 'none';
+    sucess.style.display = 'block';
+  } else {
+    input.classList.add('error');
+    errorMessage.style.display = 'block';
+  }
+}
